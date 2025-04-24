63bf85a835c5c548a448e3fb32fa1517705c28ce
almanongeeyan <153313413+almanongeeyan@users.noreply.github.com>
2025-04-25 00:51:04 +0800

    Update library_db.sql

--- python/library_db.sql
+++ python/library_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 24, 2025 at 06:13 PM
+-- Generation Time: Apr 24, 2025 at 06:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,6 +48,54 @@ INSERT INTO `archive` (`archive_id`, `book_id`, `title`, `genre`, `author`, `pub
 (1, 1, 'Free', 'Horror', 'Gee', '2002-04-23', 'Free', 'Missing', 'Fountain', '2025-04-24 23:52:30'),
 (2, 2, 'Cook', 'Recipes', 'Kolin', '2022-12-21', 'Borrowed', 'Lost', 'School', '2025-04-24 23:54:01');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(8) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `published_date` date NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `title`, `genre`, `author`, `published_date`, `status`) VALUES
+(3, 'Book of Lady Gaga', 'Punk', 'Lady Gaga', '2025-01-21', 'Borrowed'),
+(4, 'Taylor\'s Diary', 'Magical', 'Taylor Swift Batumbakal', '2012-02-21', 'Free'),
+(5, 'How to use Kagura', 'Games', 'Gyan Gee Almanon', '2025-04-11', 'Free'),
+(6, 'Lovey Dovey', 'Cutesy', 'Aliyah Policarpio', '2017-04-05', 'Borrowed'),
+(10, 'Ikaw Ay Ako', 'Drama', 'Klarisse and Morissette', '2012-05-12', 'Free');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowers`
+--
+
+CREATE TABLE `borrowers` (
+  `borrow_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `borrower_name` varchar(255) NOT NULL,
+  `borrow_date` date NOT NULL,
+  `due_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrowers`
+--
+
+INSERT INTO `borrowers` (`borrow_id`, `book_id`, `borrower_name`, `borrow_date`, `due_date`) VALUES
+(1, 6, 'Aliyah', '2025-04-24', '2025-05-08'),
+(2, 3, 'Mistress Isabelle Brooks', '2025-04-25', '2025-05-09');
+
 --
 -- Indexes for dumped tables
 --
@@ -59,6 +107,19 @@ ALTER TABLE `archive`
   ADD PRIMARY KEY (`archive_id`);
 
 --
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `borrowers`
+--
+ALTER TABLE `borrowers`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,6 +128,28 @@ ALTER TABLE `archive`
 --
 ALTER TABLE `archive`
   MODIFY `archive_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `borrowers`
+--
+ALTER TABLE `borrowers`
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrowers`
+--
+ALTER TABLE `borrowers`
+  ADD CONSTRAINT `borrowers_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
